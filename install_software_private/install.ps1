908d7df621d6ea2b3199913f1fbacff8975dafca
misteerd <misteerd@web.de>
2021-04-16 00:14:01 +0200

    added bunch of stuff variables for rows,lines started to implement the automated downloads

--- install_software_private/install.ps1
+++ install_software_private/install.ps1
@@ -1,6 +1,10 @@
 #elevate as admin
 # if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
-# { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+# { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File "$PSCommandPath"" -Verb RunAs; exit }
+
+#set variables for scriptpath
+$scriptpath = $MyInvocation.MyCommand.Definition
+$scriptfolder = [System.IO.Path]::GetDirectoryName($scriptpath)
 
 ##create window
 #create window itself
@@ -19,83 +23,121 @@ $info_text.text = "Which Software you want to install?"
 $info_text.AutoSize = $true
 $objForm.Controls.Add($info_text)
 
+#set line and row
+$row1 = "40"
+$row2 = "190"
+$row3 = "340"
+$line1 = "50"
+$line2 = "70"
+$line3 = "90"
+$line4 = "110"
+$line5 = "130"
+$line6 = "150"
+$line7 = "170"
+$line8 = "190"
+$line9 = "210"
+
+#set name software name size
+$name_size = "150,20"
+
 #Create the collection of checkboxes
 $check_7zip = New-Object System.Windows.Forms.CheckBox
-$check_7zip.Location = '40,50'
-$check_7zip.size = '150,20'
+$check_7zip.Location = "$row1,$line1"
+$check_7zip.size = $name_size
 $check_7zip.Checked = $true 
 $check_7zip.Text = "Install 7-Zip"
 $objForm.Controls.Add($check_7zip)
 
 $check_battlenet = New-Object System.Windows.Forms.CheckBox
-$check_battlenet.Location = '40,70'
+$check_battlenet.Location = "$row1,$line2"
 $check_battlenet.size = '150,20'
 $check_battlenet.Checked = $false 
 $check_battlenet.Text = "Install Battle.Net"
 $objForm.Controls.Add($check_battlenet)
 
 $check_chrome = New-Object System.Windows.Forms.CheckBox
-$check_chrome.Location = '40,90'
+$check_chrome.Location = "$row1,$line3"
 $check_chrome.size = '150,20'
 $check_chrome.Checked = $false 
 $check_chrome.Text = "Install Chrome"
 $objForm.Controls.Add($check_chrome)
 
 $check_discord = New-Object System.Windows.Forms.CheckBox
-$check_discord.Location = '40,110'
+$check_discord.Location = "$row1,$line4"
 $check_discord.size = '150,20'
 $check_discord.Checked = $false 
 $check_discord.Text = "Install Discord"
 $objForm.Controls.Add($check_discord)
 
 $check_epicgames = New-Object System.Windows.Forms.CheckBox
-$check_epicgames.Location = '40,130'
+$check_epicgames.Location = "$row1,$line5"
 $check_epicgames.size = '150,20'
 $check_epicgames.Checked = $false 
 $check_epicgames.Text = "Install EpicGames"
 $objForm.Controls.Add($check_epicgames)
 
 $check_firefox = New-Object System.Windows.Forms.CheckBox
-$check_firefox.Location = '40,150'
+$check_firefox.Location = "$row1,$line6"
 $check_firefox.size = '150,20'
 $check_firefox.Checked = $false 
 $check_firefox.Text = "Install Firefox"
 $objForm.Controls.Add($check_firefox)
 
 $check_libreoffice = New-Object System.Windows.Forms.CheckBox
-$check_libreoffice.Location = '40,170'
+$check_libreoffice.Location = "$row1,$line7"
 $check_libreoffice.size = '150,20'
 $check_libreoffice.Checked = $false 
 $check_libreoffice.Text = "Install LibreOffice"
 $objForm.Controls.Add($check_libreoffice)
 
 $check_npp = New-Object System.Windows.Forms.CheckBox
-$check_npp.Location = '190,50'
+$check_npp.Location = "$row2,$line1"
 $check_npp.size = '150,20'
 $check_npp.Checked = $true 
 $check_npp.Text = "Install Notepad++"
 $objForm.Controls.Add($check_npp)
 
 $check_steam = New-Object System.Windows.Forms.CheckBox
-$check_steam.Location = '190,70'
+$check_steam.Location = "$row2,$line2"
 $check_steam.size = '150,20'
 $check_steam.Checked = $false 
 $check_steam.Text = "Install Steam"
 $objForm.Controls.Add($check_steam)
 
 $check_thunderbird = New-Object System.Windows.Forms.CheckBox
-$check_thunderbird.Location = '190,90'
+$check_thunderbird.Location = "$row2,$line3"
 $check_thunderbird.size = '150,20'
 $check_thunderbird.Checked = $false 
 $check_thunderbird.Text = "Install Thunderbird"
 $objForm.Controls.Add($check_thunderbird)
 
-$check_thunderbird = New-Object System.Windows.Forms.CheckBox
-$check_thunderbird.Location = '340,90'
-$check_thunderbird.size = '150,20'
-$check_thunderbird.Checked = $false 
-$check_thunderbird.Text = "Install Thunderbird"
-$objForm.Controls.Add($check_thunderbird)
+$check_vlc = New-Object System.Windows.Forms.CheckBox
+$check_vlc.Location = "$row3,$line3"
+$check_vlc.size = '150,20'
+$check_vlc.Checked = $false 
+$check_vlc.Text = "Install VLC"
+$objForm.Controls.Add($check_vlc)
+
+$check_teamviewer = New-Object System.Windows.Forms.CheckBox
+$check_teamviewer.Location = "$row3,$line1"
+$check_teamviewer.size = '150,20'
+$check_teamviewer.Checked = $false 
+$check_teamviewer.Text = "Install Teamviewer"
+$objForm.Controls.Add($check_teamviewer)
+
+$check_pdf24 = New-Object System.Windows.Forms.CheckBox
+$check_pdf24.Location = "$row3,$line2"
+$check_pdf24.size = '150,20'
+$check_pdf24.Checked = $false 
+$check_pdf24.Text = "Install PDF24"
+$objForm.Controls.Add($check_pdf24)
+
+$check_obs = New-Object System.Windows.Forms.CheckBox
+$check_obs.Location = "$row2,$line4"
+$check_obs.size = '150,20'
+$check_obs.Checked = $false 
+$check_obs.Text = "Install OBS"
+$objForm.Controls.Add($check_obs)
 
 #create install button
 $start_ins_button = New-Object System.Windows.Forms.Button
@@ -121,7 +163,11 @@ $start_ins_button.Add_Click(
         $check_libreoffice.Checked, 
         $check_npp.Checked, 
         $check_steam.Checked, 
-        $check_thunderbird.Checked
+        $check_thunderbird.Checked,
+        $check_vlc.Checked,
+        $check_teamviewer,
+        $check_pdf24,
+        $check_obs
     ) 
     $objForm.Close()
     }
@@ -156,82 +202,162 @@ $objForm.Controls.AddRange(@(
     $check_npp, 
     $check_steam, 
     $check_thunderbird,
+    $check_vlc,
+    $check_teamviewer,
+    $check_pdf24,
+    $check_obs,
     $start_ins_button,
     $cancel_button)
 )
 
-#open window
-[void] $objForm.ShowDialog()
+
 function start_install {
     param ()
     if ($check_7zip.Checked -eq $true) {
-        Write-Host "7-Zip installation startet ..."
         install_7zip
     }
     if ($check_battlenet.Checked -eq $true) {
-        Write-Host "Battle.Net installation startet ..."
         install_battlenet
     }
     if ($check_chrome.Checked -eq $true) {
-        Write-Host "Google Crome installation startet ..."
         install_chrome
     }
     if ($check_discord.Checked -eq $true) {
-        Write-Host "Discord installation startet ..."
         install_discord
     }
     if ($check_epicgames.Checked -eq $true) {
-        Write-Host "Epic Games installation startet ..."
         install_epicgames
     }
     if ($check_firefox.Checked -eq $true) {
-        Write-Host "Mozilla Firefox installation startet ..."
         install_firefox
     }
     if ($check_libreoffice.Checked -eq $true) {
-        Write-Host "LibreOffice installation startet ..."
         install_libreoffice
     }
     if ($check_npp.Checked -eq $true) {
-        Write-Host "Notepad++ installation startet ..."
         install_npp
     }
     if ($check_steam.Checked -eq $true) {
-        Write-Host "Steam installation startet ..."
         install_steam
     }
     if ($check_thunderbird.Checked -eq $true) {
-        Write-Host "Thunderbird installation startet ..."
         install_thunderbird
-    }   
+    }
+    if ($check_vlc.Checked -eq $true) {
+        install_vlc
+    }
+    if ($check_teamviewer.Checked -eq $true) {
+        install_teamviewer
+    }
+    if ($check_pdf24.Checked -eq $true) {
+        install_pdf24
+    }
+    if ($check_obs.Checked -eq $true) {
+        install_obs
+    }
 }
 function install_7zip {
+    Write-Host "7-Zip installation startet ..."
 
+    $7zip_URL = 'https://www.7-zip.org/download.html'
+    $7zip_page = Invoke-RestMethod -Uri $7zip_URL
+    $7zip_DLPrefix = $7zip_URL.Split('d')[0]
+    $7zip_RelLink = (((($7zip_page -split '7-Zip for 64-bit Windows')[1] -split '<TR>')[-1] -split 'href="')[1] -split '"')[0]
+
+    #put information together for downloadlink
+    $7zip_download_path = -join ($7zip_DLPrefix, $7zip_RelLink)
+
+    #get the installer filename
+    $7zip_installer_file = ((((($7zip_page -split '7-Zip for 64-bit Windows')[1] -split '<TR>')[-1] -split 'href="')[1] -split '"')[0] -split "a/")[1]
+
+    #download 7zip installer
+    (New-Object System.Net.WebClient).DownloadFile($7zip_download_path,"$scriptfolder\$7zip_installer_file")
+
+    #start installation of 7zip
+    #Start-Process -FilePath "msiexec.exe" -ArgumentList "/i ""$ScriptFolder\$7zip_installer_file"" /qn /norestart" -Wait -WindowStyle Hidden
+    Start-Sleep -Seconds "5"
 }
 function install_battlenet {
-    
+    Write-Host "Battle.Net installation startet ..."
 }
 function install_chrome {
-    
+    Write-Host "Google Crome installation startet ..."
 }
 function install_discord {
-       
+    Write-Host "Discord installation startet ..."   
 }
 function install_epicgames {
-    
+    Write-Host "Epic Games installation startet ..."
 }
 function install_firefox {
-    
+    Write-Host "Mozilla Firefox installation startet ..."
 }
 function install_libreoffice {
-    
+    Write-Host "LibreOffice installation startet ..."
 }
 function install_npp {
+    Write-Host "Notepad++ installation startet ..."
+    $github_api_url_npp = "https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases/latest"
+    $npp_downloads_list = (Invoke-WebRequest $github_api_url_npp | ConvertFrom-Json).assets.browser_download_url
+    foreach ($item in $npp_downloads_list){
+        if ($item -like "*x64.exe"){
+            $npp_download_path = $item
+        }
+    }
+    $npp_installer_file = ($npp_download_path -split "/")[-1]
     
+    #download the npp installer
+    (New-Object System.Net.WebClient).DownloadFile($npp_download_path,"$scriptfolder\$npp_installer_file")
+
+    #start installation for npp
+    #Start-Process -FilePath "$scriptfolder\$npp_installer_file" -ArgumentList "-ms" -Wait -WindowStyle Hidden
+    Start-Sleep -Seconds "5"
 }
 function install_steam {
-    
+    Write-Host "Steam installation startet ..."
 }
 function install_thunderbird {
+    Write-Host "Thunderbird installation startet ..."
+    #get newest version and create downloadpath
+    $thunderbird_newest_version = (Invoke-WebRequest "https://product-details.mozilla.org/1.0/thunderbird_versions.json" | ConvertFrom-Json).LATEST_THUNDERBIRD_VERSION
+    $thunderbird_download_path = "https://download.mozilla.org/?product=thunderbird-$thunderbird_newest_version-SSL&os=win&lang=de"
+    $thunderbird_installer_file = "Thunderbird_$thunderbird_newest_version.exe"
+
+    #download the thunderbird installer
+    (New-Object System.Net.WebClient).DownloadFile($thunderbird_download_path,"$scriptfolder\$thunderbird_installer_file")
     
+    #start installation for thunderbird
+    #Start-Process -FilePath "$scriptfolder\$thunderbird_installer_file" -ArgumentList "-ms" -Wait -WindowStyle Hidden
+    Start-Sleep -Seconds "5"
 }
+function install_vlc {
+    Write-Host "VLC installation startet ..."
+}
+function install_teamviewer {
+    Write-Host "Teamviewer installation startet ..."
+}
+function install_pdf24 {
+    Write-Host "PDF24 installation startet ..."
+
+    $URL = 'https://tools.pdf24.org/de/creator'
+    $pdf24_Page = Invoke-RestMethod -Uri $url
+    $dllinkpart_pdf24 = (((($pdf24_Page -split "business")[-1] -split "',updateUrl:")[0] -split "//")[1] -split "{")[0]
+    $newest_version_pdf24 = ((($pdf24_Page -split "Aktuelle Version</div>")[1] -split "</span>")[0] -split "Version ")[1]
+    $pdf24_download_path = "https://" + $dllinkpart_pdf24 + $newest_version_pdf24
+    $pdf24_installer_file = "pdf24-creator-$newest_version_pdf24.msi"
+
+    #download the pdf24 installer
+    (New-Object System.Net.WebClient).DownloadFile($pdf24_download_path,"$scriptfolder\$pdf24_installer_file")
+
+    #start installation for psf24
+    #Start-Process -FilePath "msiexec.exe" -ArgumentList "/i ""$ScriptFolder\$installer_file"" /qn /norestart" -Wait -WindowStyle Hidden
+    Start-Sleep -Seconds "5"
+}
+function install_obs {
+    Write-Host "OBS installation startet ..."
+}
+
+
+#open window
+[void] $objForm.ShowDialog()
+Start-Sleep -Seconds "10"
